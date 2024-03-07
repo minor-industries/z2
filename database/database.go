eeb6a4e5d1027ddcb98b9f610cff98642a242c63
suurkivi <136639517+suurkivi@users.noreply.github.com>
2024-03-07 13:01:42 -0800

    WIP: bike data

---
+++ database/database.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"github.com/glebarez/sqlite"
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
+	"gorm.io/gorm"
+	"time"
+)
+
+type Value struct {
+	ID        uuid.UUID `gorm:"type:char(36);primary_key"`
+	Timestamp time.Time
+	Value     float64
+	SeriesID  uint16
+	Series    *Series `gorm:"foreignKey:SeriesID"`
+}
+
+type Series struct {
+	ID   uint16 `gorm:"primaryKey"`
+	Name string `gorm:"unique"`
+	Unit string
+}
+
+func Get(filename string) (*gorm.DB, error) {
+	db, err := gorm.Open(sqlite.Open(filename), &gorm.Config{})
+	if err != nil {
+		return nil, errors.Wrap(err, "open")
+	}
+
+	for _, table := range []any{
+		&Value{},
+		&Series{},
+	} {
+		err = db.AutoMigrate(table)
+		if err != nil {
+			return nil, errors.Wrap(err, "migrate measurement")
+		}
+	}
+	return db, nil
+}
---
+++ database/database_test.go
@@ -0,0 +1,101 @@
+package database_test
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/minor-industries/codelab/cmd/bike/database"
+	"github.com/stretchr/testify/require"
+	"gorm.io/gorm"
+	"os"
+	"testing"
+	"time"
+)
+
+/*
+InstantSpeed: (float64) 35.6,
+ AverageSpeed: (float64) 0,
+ InstantCadence: (float64) 54,
+ AverageCadence: (float64) 0,
+ TotalDistance: (float64) 140,
+ ResistanceLevel: (float64) 0,
+ InstantPower: (float64) 200,
+ AveragePower: (float64) 0,
+ TotalEnergy: (float64) 3,
+ EnergyPerHour: (float64) 0,
+ EnergyPerMinute: (float64) 0,
+ HeartRate: (float64) 0,
+ MetabolicEquivalent: (float64) 0,
+ ElapsedTime: (float64) 0,
+ RemainingTime: (float64) 0
+
+instant_speed: (float64) 35.6,
+average_speed: (float64) 0,
+instant_cadence: (float64) 54,
+average_cadence: (float64) 0,
+total_distance: (float64) 140,
+resistance_level: (float64) 0,
+instant_power: (float64) 200,
+average_power: (float64) 0,
+total_energy: (float64) 3,
+energy_per_hour: (float64) 0,
+energy_per_minute: (float64) 0,
+heart_rate: (float64) 0,
+metabolic_equivalent: (float64) 0,
+elapsed_time: (float64) 0,
+remaining_time: (float64) 0
+*/
+
+func TestGet(t *testing.T) {
+	dbname := os.ExpandEnv("$HOME/bike.db")
+	fmt.Println(dbname)
+	db, err := database.Get(dbname)
+	require.NoError(t, err)
+
+	seriesMap := loadSeries(db)
+
+	t.Run("create types", func(t *testing.T) {
+		names := []string{
+			"bike_instant_speed",
+			"bike_instant_cadence",
+			"bike_total_distance",
+			"bike_instant_power",
+			"bike_total_energy",
+		}
+
+		for _, name := range names {
+			if _, found := seriesMap[name]; found {
+				continue
+			}
+			db.Create(&database.Series{
+				Name: name,
+				Unit: "",
+			})
+		}
+	})
+
+	seriesMap = loadSeries(db)
+
+	t.Run("measurement", func(t *testing.T) {
+		m := &database.Value{
+			ID:        uuid.New(),
+			Timestamp: time.Now(),
+			Value:     55,
+			Series:    seriesMap["bike_instant_cadence"],
+		}
+		tx := db.Create(m)
+		require.NoError(t, tx.Error)
+	})
+}
+
+func loadSeries(db *gorm.DB) map[string]*database.Series {
+	typeMap := map[string]*database.Series{}
+	{
+		var types []*database.Series
+		db.Find(&types)
+
+		for _, mt := range types {
+			typeMap[mt.Name] = mt
+		}
+	}
+	return typeMap
+}
