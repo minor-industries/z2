9d092f4080ad0bbd40de4bbd5b08a3a1a7d713fd
suurkivi <136639517+suurkivi@users.noreply.github.com>
2024-03-11 13:45:50 -0700

    WIP: rower source

--- main_darwin.go
+++ main_darwin.go
@@ -1,3 +1,4 @@
 package main
 
-const address = "5f953f6e-382c-39f7-c5ce-643b6141c967"
+// const address = "5f953f6e-382c-39f7-c5ce-643b6141c967" // IC bike
+const address = "41f94de4-2cb8-a329-d685-2644a65213ff" // PM5 431130895 Row
