d3092027bb5a33016c5b916aaf0025e4376f5ef1
suurkivi <136639517+suurkivi@users.noreply.github.com>
2024-06-15 06:13:14 -0700

    WIP: detect workout

---
+++ app/detect_workout_test.go
@@ -0,0 +1,40 @@
+package app
+
+import (
+	"fmt"
+	"github.com/minor-industries/rtgraph/database"
+	"github.com/minor-industries/rtgraph/schema"
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+	"time"
+)
+
+type Detector struct {
+	active bool
+}
+
+func (d Detector) Detect(values []schema.Value) {
+	for _, v := range values {
+		_ = v
+	}
+}
+
+func Test_DetectWorkout(t *testing.T) {
+	dbPath := os.ExpandEnv("$HOME/.z2/z2.db")
+	errCh := make(chan error)
+
+	db, err := database.Get(dbPath, errCh)
+	require.NoError(t, err)
+
+	window, err := db.LoadDataWindow("bike_instant_speed", time.Time{})
+	require.NoError(t, err)
+
+	d := &Detector{}
+
+	d.Detect(window.Values)
+
+	for i, value := range window.Values {
+		fmt.Println(i, value.Timestamp, value.Value)
+	}
+}
