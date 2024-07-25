e5be0d95166c731408096a75478662086cc44fe5
suurkivi <136639517+suurkivi@users.noreply.github.com>
2024-07-25 12:55:05 -0700

    WIP: backup

--- backup/backup.go
+++ backup/backup.go
@@ -1,19 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"github.com/minor-industries/rtgraph/database"
 	"github.com/pkg/errors"
 	"os"
+	"path/filepath"
 )
 
-func main() {
-	dbPath := os.ExpandEnv("$HOME/.z2/z2.db")
+func run() error {
+	z2Path := os.ExpandEnv("$HOME/.z2")
+	dbPath := filepath.Join(z2Path, "z2.db")
 
-	errCh := make(chan error)
-
-	db, err := database.Get(dbPath, errCh)
+	db, err := database.Get(dbPath)
 	if err != nil {
 		return errors.Wrap(err, "get database")
 	}
 
+	backupFile := filepath.Join(z2Path, "z2-backup.db")
+	tx := db.GetORM().Exec("VACUUM INTO ?", backupFile)
+	if tx.Error != nil {
+		return errors.Wrap(tx.Error, "vacuum into")
+	}
+
+	backupPath := filepath.Join(z2Path, "backup")
+	err = os.MkdirAll(backupPath, 0o750)
+	if err != nil {
+		return errors.Wrap(err, "mkdir backup path")
+	}
+
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
 }
