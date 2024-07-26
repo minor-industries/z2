b5ee72d343542e3413000ac9e3cf841429090e90
suurkivi <136639517+suurkivi@users.noreply.github.com>
2024-07-26 10:53:30 -0700

    add missing file

---
+++ cfg/cfg.go
@@ -0,0 +1,46 @@
+package cfg
+
+import (
+	"github.com/BurntSushi/toml"
+	"github.com/pkg/errors"
+	"os"
+)
+
+type Config struct {
+	Source            string   `toml:"source"`
+	ReplayDB          string   `toml:"replay_db"`
+	Port              int      `toml:"port"`
+	HeartrateMonitors []string `toml:"heartrate_monitors"`
+	StaticPath        string   `toml:"static_path"`
+	RemoveDB          bool     `toml:"remove_db"`
+	Webview           bool     `toml:"webview"`
+	XRes              int      `toml:"xres"`
+	YRes              int      `toml:"yres"`
+	Scan              bool     `toml:"scan"`
+	Audio             string   `toml:"audio"`
+
+	BackupPrefix string `toml:"backup_prefix"`
+
+	Devices map[string]string `toml:"devices"`
+}
+
+var Default = Config{
+	Port:    8077,
+	Webview: true,
+	XRes:    1132,
+	YRes:    700,
+	Audio:   "browser",
+}
+
+const (
+	DefaultConfigPath = "$HOME/.z2/config.toml"
+)
+
+func Load(path string) (*Config, error) {
+	opts := Default
+	cfgFile := os.ExpandEnv(path)
+	if _, err := toml.DecodeFile(cfgFile, &opts); err != nil {
+		return nil, errors.Wrap(err, "decode file")
+	}
+	return &opts, nil
+}
