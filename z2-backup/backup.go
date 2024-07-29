e8dcd5e3172ef5941ec4a8486dfcb8ee44ae078f
suurkivi <136639517+suurkivi@users.noreply.github.com>
2024-07-29 11:48:07 -0700

    cleanup backup script for use in restic

--- z2-backup/backup.go
+++ z2-backup/backup.go
@@ -1,41 +1,13 @@
 package main
 
 import (
-	"compress/gzip"
 	"fmt"
 	"github.com/minor-industries/rtgraph/database"
 	"github.com/minor-industries/z2/cfg"
 	"github.com/pkg/errors"
-	"io"
 	"os"
 	"path/filepath"
-	"strings"
-	"time"
 )
-
-func gzipFile(src, dest string) error {
-	srcFile, err := os.Open(src)
-	if err != nil {
-		return errors.Wrap(err, "open source file")
-	}
-	defer srcFile.Close()
-
-	destFile, err := os.Create(dest)
-	if err != nil {
-		return errors.Wrap(err, "create destination file")
-	}
-	defer destFile.Close()
-
-	gzipWriter := gzip.NewWriter(destFile)
-	defer gzipWriter.Close()
-
-	_, err = io.Copy(gzipWriter, srcFile)
-	if err != nil {
-		return errors.Wrap(err, "copy to gzip")
-	}
-
-	return nil
-}
 
 func removeIfExist(path string) error {
 	if _, err := os.Stat(path); err == nil {
@@ -67,57 +39,25 @@ func run() error {
 	}
 
 	z2Path := os.ExpandEnv("$HOME/.z2")
-	dbPath := filepath.Join(z2Path, "z2.db")
-
-	db, err := database.Get(dbPath)
-	if err != nil {
-		return errors.Wrap(err, "get database")
-	}
+	dbFile := filepath.Join(z2Path, "z2.db")
+	backupPath := filepath.Join(z2Path, "backup")
 
 	prefixed := fmt.Sprintf("z2-backup-%s", opts.BackupPrefix)
+	backupFile := filepath.Join(backupPath, prefixed+".db")
 
-	backupFile := filepath.Join(z2Path, prefixed+".db")
-
-	if _, err := os.Stat(backupFile); err == nil {
-		if err := os.Remove(backupFile); err != nil {
-			return errors.Wrap(err, "remove existing backup file")
-		}
-	}
-
-	tx := db.GetORM().Exec("VACUUM INTO ?", backupFile)
-	if tx.Error != nil {
-		return errors.Wrap(tx.Error, "vacuum into")
-	}
-
-	backupPath := filepath.Join(z2Path, "backup")
 	err = os.MkdirAll(backupPath, 0o750)
 	if err != nil {
 		return errors.Wrap(err, "mkdir backup path")
 	}
 
-	gzippedBackupFile := filepath.Join(backupPath, prefixed+".db.gz")
-	if err := removeIfExist(gzippedBackupFile); err != nil {
-		return errors.Wrap(err, "remove existing gzipped backup file")
-	}
-	if err := gzipFile(backupFile, gzippedBackupFile); err != nil {
-		return errors.Wrap(err, "gzip backup file")
+	db, err := database.Get(dbFile)
+	if err != nil {
+		return errors.Wrap(err, "get database")
 	}
 
-	if false {
-		now := time.Now()
-		_, week := now.ISOWeek()
-		dayOfWeek := strings.ToLower(now.Format("Mon"))
-
-		weekBackupFile := filepath.Join(backupPath, fmt.Sprintf("%s-week-%02d.db.gz", prefixed, week))
-		dayBackupFile := filepath.Join(backupPath, fmt.Sprintf("%s-%s.db.gz", prefixed, dayOfWeek))
-
-		if err := hardLinkFile(gzippedBackupFile, weekBackupFile); err != nil {
-			return errors.Wrap(err, "create hard link for week backup")
-		}
-
-		if err := hardLinkFile(gzippedBackupFile, dayBackupFile); err != nil {
-			return errors.Wrap(err, "create hard link for day backup")
-		}
+	tx := db.GetORM().Exec("VACUUM INTO ?", backupFile)
+	if tx.Error != nil {
+		return errors.Wrap(tx.Error, "vacuum into")
 	}
 
 	return nil
