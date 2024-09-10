f355ea1a9c097f5ffc04be6a41bd43875bca989c
suurkivi <136639517+suurkivi@users.noreply.github.com>
2024-09-10 14:35:13 -0700

    WIP: static demo

--- static/env.js
+++ static/env.js
@@ -18,12 +18,18 @@ export async function getEnv() {
         },
         async loadDataBetween() {
             return [];
+        },
+        async insertValue() {
         }
     }; // TODO
 
     await runGoWasm("/dist/z2.wasm")
 
     const connector = new WASMConnector(window.subscribe);
+
+    setInterval(function () {
+        window.createValue("heartrate", (new Date()).getTime(), 70.0 + Math.random());
+    }, 1000)
 
     return {
         apiClient: window.goWasmApi,
