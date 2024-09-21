80b88e694d6f06aa00c51f5698ab84c9e80fd519
suurkivi <136639517+suurkivi@users.noreply.github.com>
2024-09-21 07:36:37 -0700

    refactor

--- sync/sync.go
+++ sync/sync.go
@@ -11,30 +11,6 @@ import (
 	"time"
 )
 
-type NamedSeries struct {
-	Name       string
-	Timestamps []int64
-	Values     []float64
-}
-
-var namedSampleData = []NamedSeries{
-	{
-		Name:       "Series A",
-		Timestamps: []int64{1627689945, 1627689955, 1627689965},
-		Values:     []float64{1.23, 2.34, 3.45},
-	},
-	{
-		Name:       "Series B",
-		Timestamps: []int64{1627689975, 1627689985, 1627689995},
-		Values:     []float64{4.56, 5.67, 6.78},
-	},
-	{
-		Name:       "Series A",
-		Timestamps: []int64{1627689965, 1627690005, 1627690015}, // Overlap with 1627689965, new unique timestamps
-		Values:     []float64{7.89, 8.90, 9.01},
-	},
-}
-
 func insertSeriesBatchWithTransaction(db *gorm.DB, series NamedSeries) (int, error) {
 	count := 0
 
@@ -90,15 +66,7 @@ func run() error {
 		return nil
 	})
 	if err != nil {
-		return errors.Wrap(err, "")
-	}
-
-	for _, s := range namedSampleData {
-		count, err := insertSeriesBatchWithTransaction(dst.GetORM(), s)
-		if err != nil {
-			return errors.Wrap(err, "insert")
-		}
-		fmt.Println(s.Name, s, count)
+		return errors.Wrap(err, "bucket all")
 	}
 
 	return nil
