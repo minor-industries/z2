91f00d73d77bf6b5db1002ea1554325a0de4ed8d
suurkivi <136639517+suurkivi@users.noreply.github.com>
2024-09-22 08:47:10 -0700

    save series names to db

--- sync/server.go
+++ sync/server.go
@@ -1,8 +1,10 @@
 package sync
 
 import (
+	"github.com/chrispappas/golang-generics-set/set"
 	"github.com/gin-gonic/gin"
 	"github.com/minor-industries/rtgraph/database/sqlite"
+	"github.com/pkg/errors"
 	"github.com/tinylib/msgp/msgp"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
@@ -14,12 +16,22 @@ type SyncResponse struct {
 	NewItems      int `json:"new_items"`
 }
 
-func insertSeriesBatchWithTransaction(db *gorm.DB, series NamedSeries) (int, error) {
+func insertSeriesBatchWithTransaction(
+	db *gorm.DB,
+	series NamedSeries,
+) (int, error) {
 	count := 0
 
 	err := db.Transaction(func(tx *gorm.DB) error {
+		seen := set.Set[string]{}
+
 		for i := range series.Timestamps {
 			id := sqlite.HashedID(series.Name)
+
+			if err := addSeriesName(tx, seen, series.Name, id); err != nil {
+				return errors.Wrap(err, "add series name")
+			}
+
 			row := sqlite.Sample{
 				SeriesID:  id,
 				Timestamp: series.Timestamps[i],
@@ -48,8 +60,28 @@ func insertSeriesBatchWithTransaction(db *gorm.DB, series NamedSeries) (int, err
 	return count, nil
 }
 
+func addSeriesName(tx *gorm.DB, seen set.Set[string], seriesName string, id []byte) error {
+	if !seen.Has(seriesName) {
+		res := tx.Clauses(clause.OnConflict{
+			DoNothing: true,
+		}).Create(&sqlite.Series{
+			ID:   id,
+			Name: seriesName,
+		})
+
+		if res.Error != nil {
+			return errors.Wrap(res.Error, "create")
+		}
+
+		seen.Add(seriesName)
+	}
+
+	return nil
+}
+
 func RunServer(db *sqlite.Backend) error {
 	r := gin.Default()
+
 	r.POST("/sync", func(c *gin.Context) {
 		var series NamedSeries
 		if err := msgp.Decode(c.Request.Body, &series); err != nil {
