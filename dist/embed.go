f8075a73dfc7467920f68def7873d7d8c04347b1
suurkivi <136639517+suurkivi@users.noreply.github.com>
2024-09-23 23:18:21 -0700

    first commit

---
+++ dist/embed.go
@@ -0,0 +1,12 @@
+package dist
+
+import (
+	"embed"
+)
+
+//go:embed *.js
+//go:embed css/*.css
+//go:embed css/purecss/*.css
+//go:embed html/*.html
+//go:embed sounds/*.mp3
+var FS embed.FS
---
+++ env.js
@@ -0,0 +1,44 @@
+import {runGoWasm, runOnce, WASMConnector} from "./dist/z2-bundle.js";
+
+async function maybeStartFrontendBLE() {
+    const url = new URL(window.location.href);
+    const pathParts = url.pathname.split('/');
+    const currentPage = pathParts[pathParts.length - 1];
+
+    switch (currentPage) {
+        case 'bike.html':
+            window.z2GoWasm.z2.startReplay("bike");
+            window.z2GoWasm.z2.startApp("bike");
+            return;
+    }
+}
+
+async function setup() {
+    console.log("running setup");
+    window.dbManager = {
+        async loadDataAfter() {
+            return [];
+        },
+        async loadDataBetween() {
+            return [];
+        },
+        async insertValue() {
+        },
+        async allSeriesNames() {
+            return [];
+        },
+    }; // TODO
+
+    await runGoWasm("/dist/z2.wasm")
+
+    return {
+        apiClient: window.z2GoWasm.apiClient,
+        calendarClient: window.z2GoWasm.calendarClient,
+        connector: new WASMConnector(window.z2GoWasm.z2.subscribe),
+        streamEvents: window.z2GoWasm.z2.streamEvents,
+        maybeStartFrontendBLE,
+        sync: window.z2GoWasm.z2.triggerSync,
+    };
+}
+
+export const getEnv = runOnce(setup);
