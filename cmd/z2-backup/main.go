f023686153e7aa3e9c1becd0e19c11cfd2c269c6
suurkivi <136639517+suurkivi@users.noreply.github.com>
2024-09-25 08:18:26 -0700

    callback

--- cmd/z2-backup/main.go
+++ cmd/z2-backup/main.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"fmt"
-	"github.com/davecgh/go-spew/spew"
 	"github.com/minor-industries/z2/backup"
+	"math"
 	"os"
 )
 
 func main() {
+	lastQuantum := math.MaxFloat64
+
 	if err := backup.Run(func(msg any) error {
-		fmt.Println(spew.Sdump(msg))
+		switch msg := msg.(type) {
+		case backup.ResticStatus:
+			if msg.PercentDone < lastQuantum {
+				lastQuantum = 0.0
+			}
+
+			currentQuantum := float64(int(msg.PercentDone*10)) / 10.0
+
+			if currentQuantum > lastQuantum {
+				lastQuantum = currentQuantum
+				fmt.Printf("Progress: %.1f%%\n", msg.PercentDone*100)
+			}
+		case backup.ResticSummary:
+			fmt.Println("Backup done!")
+		default:
+			fmt.Println("Unknown message type")
+		}
 		return nil
 	}); err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
